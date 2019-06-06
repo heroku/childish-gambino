2ca3e87ff9e214b51412ea9c86ffea7aec31895e
Eric Chen <echen1@salesforce.com>
2019-06-06 15:06:16 -0700

    Fix CRSF issue by adhering to API only basic auth concern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,3 @@
-class ApplicationController < ActionController::Base
-  include ActionController::HttpAuthentication::Basic
-
-  http_basic_authenticate_with name: ENV.fetch("SLUG"), password: ENV.fetch("PASSWORD")
-
-  # def authenticate
-  #   #binding.pry
-  #   authenticate_with_http_basic do |username, password|
-  #     ActiveSupport::SecurityUtils.secure_compare(ENV.fetch("SLUG"), username) &
-  #     ActiveSupport::SecurityUtils.secure_compare(ENV.fetch("PASSWORD"), password)
-  #   end
-  # end
+class ApplicationController < ActionController::API
+  include HttpBasicAuth
 end
---
+++ app/controllers/concerns/http_basic_auth.rb
@@ -0,0 +1,15 @@
+module HttpBasicAuth
+  extend ActiveSupport::Concern
+  include ActionController::HttpAuthentication::Basic::ControllerMethods
+
+  included do
+    before_action :http_authenticate
+  end
+
+  def http_authenticate
+    authenticate_or_request_with_http_basic do |name, password|
+      ActiveSupport::SecurityUtils.secure_compare(name, ENV["SLUG"]) &
+        ActiveSupport::SecurityUtils.secure_compare(password, ENV["PASSWORD"])
+    end
+  end
+end
