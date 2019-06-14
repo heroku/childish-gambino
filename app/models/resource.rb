5bdc06102ca86415aedb5e529e144f3642820538
Eric Chen <echen1@salesforce.com>
2019-06-14 14:51:33 -0700

    updating config var

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -2,6 +2,6 @@ class Resource < ApplicationRecord
   attribute :access_token
   attribute :refresh_token
 
-  attr_encrypted :access_token, key: ENV["KEY"]
-  attr_encrypted :refresh_token, key: ENV["KEY"]
+  attr_encrypted :access_token, key: ENV["ENCRYPTION_KEY"]
+  attr_encrypted :refresh_token, key: ENV["ENCRYPTION_KEY"]
 end
