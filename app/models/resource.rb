f02710e2af90193b025756f4f0cc5524bc89f921
Eric Chen <echen1@salesforce.com>
2019-06-14 10:46:01 -0700

    Fixing attr_encrypted

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,4 +1,7 @@
 class Resource < ApplicationRecord
+  attribute :access_token
+  attribute :refresh_token
+
   attr_encrypted :access_token, key: ENV["KEY"]
   attr_encrypted :refresh_token, key: ENV["KEY"]
 end
